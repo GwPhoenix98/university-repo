cbd393d2f6da7f60984749068b6afbaf7d173e3b
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-23 18:32:03 +0200

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public static void main(String[] args) {
+
+        SettingFaculty facultyName = new SettingFaculty();
+
+        System.out.println("Entering the program...");
+        facultyName.setFaculty();
+    }
+}
