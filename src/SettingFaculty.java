6474bca8020082af80ad7514649c071be601f877
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-23 18:33:43 +0200

    Add faculty setting method

---
+++ src/SettingFaculty.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SettingFaculty {
+
+    boolean valid = false;
+    String faculty;
+    void setFaculty() {
+
+        try (Scanner obScanner = new Scanner(System.in)) {
+
+            System.out.print("Enter the faculty (Mathematics / Computer Science): ");
+            faculty = obScanner.nextLine()
+                    .toLowerCase()
+                    .trim()
+                    .replace(" ", "");
+
+            do {
+                switch (faculty) {
+
+                    case "mathematics":
+                        faculty = "Mathematics";
+                        System.out.println("Your faculty is " + faculty + ".");
+                        valid = true;
+                        break;
+
+                    case "computerscience":
+                        faculty = "Computer Science";
+                        System.out.println("Your faculty is " + faculty + ".");
+                        valid = true;
+                        break;
+
+                    default:
+                        System.out.println("Unknow Faculty!");
+                        System.out.print("Please enter Mathematics or Computer Science: ");
+                        faculty = obScanner.nextLine()
+                                .toLowerCase()
+                                .trim()
+                                .replace(" ", "");
+                }
+
+            } while (!valid);
+        }
+    }
+}
